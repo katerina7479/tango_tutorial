f9b2e414421fc2a318177df3776f35d916434233
Katerina Hanson <katerina.hanson@gmail.com>
2014-07-13 20:48:23 -0700

    Manage for django

--- tango/manage.py
+++ tango/manage.py
@@ -7,4 +7,8 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    args = sys.argv
+    if len(args) == 2 and args[1] == 'runserver':
+        args.append('0.0.0.0:8000')
+
     execute_from_command_line(sys.argv)
